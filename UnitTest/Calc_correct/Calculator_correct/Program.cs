ad97c52aedec0ae3e13a60a2aa41ee087788f0a2
unknown <you@example.com>
2021-10-05 11:10:49 +0300

    sds

---
+++ UnitTest/Calc_correct/Calculator_correct/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class CalculatorChecker
+    {
+        public static double ValidateCalculator(double sum1, double sum2, string operation)
+        {
+            if (operation == "+")
+            {
+                return (sum1 + sum2);
+            }
+
+            if (operation == "-")
+            {
+                return sum1 - sum2;
+
+            }
+            if (operation == "*")
+            {
+                return (sum1 * sum2);
+
+            }
+            if (operation == "/")
+            {
+                
+                try
+                {
+
+
+                    if (sum2 == 0)
+                    {
+                        
+                        throw new DivideByZeroException();
+                        
+
+                    }
+                    else
+                        return (sum1 / sum2);
+
+                }
+                catch (DivideByZeroException e) //отлов исключения
+                {
+                    Console.WriteLine($"Ошибка: {e.Message}");
+                    return 1;
+                }
+
+
+            }
+
+            return 0;
+        }
+        static public void Main()
+        {
+            double a = 0;
+            double b = 0;
+
+
+
+            Console.WriteLine("Введите число 1");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число 2");
+            b = Convert.ToDouble(Console.ReadLine());
+            double res = CalculatorChecker.ValidateCalculator(a, b, "/");
+            Console.WriteLine("res " + res);
+        }
+
+    }
+}
---
+++ UnitTest/Calc_correct/Calculator_correctTests/CalculatorCheckerTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorCheckerTests
+    {
+        [TestMethod()]
+        public void ValidateCalculatorTest_plus_ReturnTrue()
+        {
+            double a = 2;
+            double b = 4;
+            string operation = "+";
+            double result = 6;
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
+            Assert.AreEqual(result, actual);
+
+        }
+
+
+        [TestMethod]
+        public void ValidateCalculatorTest_Minus_ReturnTrue()
+        {
+            double a = 10;
+            double b = 5;
+            string operation = "-";
+            double result = 5;
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
+            Assert.AreEqual(result, actual);
+        }
+
+        [TestMethod]
+        public void ValidateCalculatorTest_multiply_ReturnTrue()
+        {
+            double a = 5;
+            double b = 5;
+            string operation = "*";
+            double result = 25;
+
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
+            Assert.AreEqual(result, actual);
+        }
+
+        [TestMethod]
+        public void ValidateCalculatorTest_divide_ReturnTrue()
+        {
+            double a = 2.5;
+            double b = 5;
+            string operation = "/";
+            double result = 0.5;
+
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
+            Assert.AreEqual(result, actual);
+        }
+
+        /*[TestMethod()]
+
+        [ExpectedException(typeof(DivideByZeroException), "Ошибка: Attempted to divide by zero.")]
+        public void ValidateCalculatorTest_divide_ReturnFalse()
+        {
+            double a = 25;
+            double b = 0;
+            
+            string operation = "/";
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
+            
+
+        }*/
+        [TestMethod()]
+        public void TestDiv_null()
+        {
+            try
+            {
+                double a = 25;
+                double b = 0;
+
+                string operation = "/";
+                double res = 1;
+                double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
+
+                Assert.AreEqual(actual,res);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+
+
+
+    }
+}

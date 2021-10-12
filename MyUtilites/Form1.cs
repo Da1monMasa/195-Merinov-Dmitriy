c3c75025c5b3cc89a76d4c1d894233223d313647
Da1monMasa <73188898+Da1monMasa@users.noreply.github.com>
2021-10-12 11:12:50 +0300

    Add files via upload

---
+++ MyUtilites/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyUtilites
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AboutProgramm_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programm" + " MyUtilites" + " contains a row with small programs, that can be useful in life ", "About program");
+        }
+    }
+}
